6a2ad8a9e15b7f2da0a0117cedde938a89365dc0
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-05 12:15:21 -0500

    Make types happen so the component will work

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+interface TableType {
+  columns: Array<{ title: string; dataProperty: string; sortable?: boolean; filterable?: boolean }>
+}
+
+const LocationTableType: any = {
+  columns: [
+    {title: "2D6", dataProperty: "roll", sortable: true, filterable: true},
+    {title: "Location", dataProperty: "location", sortable: true, filterable: true},
+  ]
+}
+
+const LocationContent: Array<{ roll: number; location: string }> = [
+  {roll: 2, location: "Atop a sleeping creature"},
+]
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.less']
 })
-export class TableComponent implements OnInit {
+export class TableComponent<A> implements OnInit {
 
-  constructor() { }
+  @Input()
+  public config: TableType;
+
+  @Input()
+  public content: any;
+
+  constructor() {
+    this.config = LocationTableType;
+    this.content = LocationContent;
+  }
 
   ngOnInit(): void {
   }
