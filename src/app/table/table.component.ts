50fec5ae73dee64f847095deef40b00cce61a0aa
CJ Harries <cj@wizardsoftheweb.pro>
2021-09-05 12:40:33 -0500

    Update table to take input

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -12,42 +12,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 
 interface TableType {
   columns: Array<{ title: string; dataProperty: string; sortable?: boolean; filterable?: boolean }>
 }
-
-const LocationTableType: any = {
-  columns: [
-    {title: "2D6", dataProperty: "roll", sortable: true, filterable: true},
-    {title: "Location", dataProperty: "location", sortable: true, filterable: true},
-  ]
-}
-
-const LocationContent: Array<{ roll: number; location: string }> = [
-  {roll: 2, location: "Atop a sleeping creature"},
-]
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.less']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnChanges {
 
   @Input()
-  public config: TableType;
+  public tableConfig: TableType = {columns:[]};
 
   @Input()
-  public content: any;
+  public tableContent: any;
 
-  constructor() {
-    this.config = LocationTableType;
-    this.content = LocationContent;
-  }
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+
   }
 
 }
